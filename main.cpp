ccae7527244a2dbb2caab96ff4275c339b247bba
zu <635aranza@gmail.com>
2024-10-30 10:59:53 -0600

    gato

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Perroooo desgraciado" << std::endl;
     printf("El printf no jala pa nada");
+    printf("El gatooo");
     return 0;
 }
