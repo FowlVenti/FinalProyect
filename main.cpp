d011e0732dde2bc8b0f266ddc57036aec93d6c0d
RubXA <pollikorub@gmail.com>
2024-10-30 11:10:48 -0600

    parte1

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Perroooo desgraciado" << std::endl;
     printf("El printf no jala pa nada");
+    if(true) {
+        std::cout << "El printf no jala pa nada";
+    }
     return 0;
 }
