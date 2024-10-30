17d998c02eb4d4e560ed42a5f7c15ee29dd3a562
RubXA <pollikorub@gmail.com>
2024-10-30 11:02:19 -0600

    no jala

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Perroooo desgraciado" << std::endl;
-    printf("El printf no jala pa nada");
+    printf("El printf no jala pa nada\n");
+    printf("Me voy a matar\n");
     return 0;
 }
