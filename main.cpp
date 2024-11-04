a7eff1585a814cdc4ab55f6450909be373b4c2e4
zu <635aranza@gmail.com>
2024-11-04 08:39:56 -0600

    gato

--- main.cpp
+++ main.cpp
@@ -7,8 +7,19 @@ int main() {
     int val=1;
     for(int fila=0; fila<5; fila++){
         for(int colum=0; colum<9; colum++){
-            matriz[fila][colum]="A";
-            //val++;
+            if(fila==3 && colum==3 || fila==3 && colum==5){
+                matriz[fila][colum]="X";
+            }
+            else if(fila==4 && colum==5){
+                matriz[fila][colum]="W";
+
+            }
+            else if(fila==4 && colum>5){
+                matriz[fila][colum]="_";
+            }
+            else{
+                matriz[fila][colum]="->";
+            }
             printf("[%s]", matriz[fila][colum].c_str());
         }
         printf("\n");
